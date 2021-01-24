e267151d536b45d49c11f668f5a491ee2433c5e3
anastasyah86 <nastasyah86@gmail.com>
2021-01-24 18:55:17 +0200

    random funct created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import './styles.css';
-
-const refs;
-
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -10,6 +7,26 @@ const colors = [
   '#009688',
   '#795548',
 ];
+
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="start"]'),
+};
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+// 1. Будет функция которая будет выбирать рандомный индекс массива
+// 2. Функция которая принимает мин и макс длину массива
+// и применяет высчитывает нужный вам индекс
+// И после цвет который соответствует этому индексу уже применяете
+const random = randomIntegerFromInterval(0, colors.length - 1);
+
+const start = function () {
+  setInterval(() => {
+    document.body.style.backgroundColor = colors[random];
+  }, 500);
+};
+// start();
+refs.startBtn.addEventListener('click', start);
+// console.log(refs.startBtn);
